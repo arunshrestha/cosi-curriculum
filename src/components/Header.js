4437c84abfa8a7571b356dbb482cd6aa816855d3
arunshrestha <arunshrestha2009@hotmail.com>
2025-03-05 13:49:19 -0500

    only merging relevant files from sherren branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,15 @@ import logo from '../assets/Brandeis_logo_seal_white_digital.png';
 const Header = () => {
     return (
         <header className="relative bg-[#003478] text-white p-4">
-            <div className="container mx-auto flex items-center justify-between">
+            <div className="container mx-auto flex flex-wrap items-center justify-center sm:justify-between text-center">
                 {/* Logo on the left */}
                 <div className="flex items-center space-x-3">
                     <img src={logo} alt="Logo" className="h-12 w-36 rounded-full" />
-                    <span className="text-xl font-semibold"></span>
                 </div>
 
                 {/* Title in the center */}
-                <h1 className="absolute left-1/2 transform -translate-x-1/2 text-2xl font-bold">
-                    Computer Science Curriculum
+                <h1 className="w-full flex-grow text-center sm:w-auto text-lg sm:text-2xl font-bold mt-2 sm:mt-0">
+                    Brandeis Computer Science Curriculum
                 </h1>
             </div>
         </header>
